4de7281b04f1d11eab045992469248850fe67d2f
Maximus Janukovich <57828861+maximusjnyk@users.noreply.github.com>
2022-09-28 11:09:53 +0300

    reInitialized empty Git repository

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ConversationsModule } from './conversations/conversations.module';
+import entities from './utils/typeorm';
+
+@Module({
+  imports: [
+    AuthModule,
+    UsersModule,
+    ConfigModule.forRoot({ envFilePath: '.env.development' }),
+    PassportModule.register({ session: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_DB_HOST,
+      port: parseInt(process.env.MYSQL_DB_PORT),
+      username: process.env.MYSQL_DB_USERNAME,
+      password: process.env.MYSQL_DB_PASSWORD,
+      database: process.env.MYSQL_DB_NAME,
+      synchronize: true,
+      entities,
+    }),
+    ConversationsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/utils/typeorm/index.ts
@@ -0,0 +1,9 @@
+import { User } from './entities/User';
+import { Session } from './entities/Session';
+import { Conversation } from './entities/Conversation';
+
+const entities = [User, Session, Conversation];
+
+export default entities;
+
+export { User, Session, Conversation };
