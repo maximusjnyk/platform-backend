c6df2170c8cdb3525c7d8057a8d9073748f6d510
Maximus Janukovich <57828861+maximusjnyk@users.noreply.github.com>
2022-09-30 10:44:10 +0300

    fix bugs

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -45,4 +45,11 @@ export class MessageService implements IMessageService {
     await this.conversationRepository.save(conversation);
     return;
   }
+  getMessagesByConversationId(conversationId: number): Promise<Message[]> {
+    return this.messageRepository.find({
+      relations: ['author'],
+      where: { conversation: { id: conversationId } },
+      order: { createdAt: 'DESC' },
+    });
+  }
 }
